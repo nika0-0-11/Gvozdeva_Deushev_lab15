8ed4a50d01f684ea50e79200a9afe703d18e1c6e
nikaaa☆ <nika_gvozdeva0.0@mail.ru>
2025-11-27 18:26:58 +0300

    Add files via upload

---
+++ Clicker_WPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace Clicker_WPF
+{
+    public partial class MainWindow : Window
+    {
+        private int _counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateCounterText();
+        }
+
+        private void UpdateCounterText()
+        {
+            CounterText.Text = $"Счётчик: {_counter}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            UpdateCounterText();
+        }
+
+        private void Button_Reset(object sender, RoutedEventArgs e)
+        {
+            _counter = 0;
+            UpdateCounterText();
+        }
+    }
+}
